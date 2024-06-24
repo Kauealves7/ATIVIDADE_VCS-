e3d4a447a05e8ee1f98a2ed643e3ed664a026e35
Kauealves7 <167824377+Kauealves7@users.noreply.github.com>
2024-06-24 19:41:20 -0300

    Add files via upload

---
+++ aula8.py
@@ -0,0 +1,7 @@
+print("progama de repetição até digitar 0")
+
+user = int(input("digite um número inteiro")) 
+while user  != 0: 
+    print("diferente de zero!!!!")
+    user = int(input("Digite um número inteiro"))
+print("fim do progama!!")    
